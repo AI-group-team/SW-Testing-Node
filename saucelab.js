71b6d23578b3c6c306141e8eaedeca403b7f5464
unknown <bereketzewde989@gmail.com>
2023-04-01 21:31:14 +0300

    test case added

--- saucelab.js
+++ saucelab.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 const webdriver = require('selenium-webdriver');
 
-// const remote = require('selenium-webdriver/remote');
-
-// Define your Sauce Labs credentials
-
-
 async function runTestWithCaps (capabilities) {
     
    const driver = new webdriver.Builder()
   .usingServer(`https://${process.env.USER_NAME}:${process.env.ACCESS_KEY}@ondemand.eu-central-1.saucelabs.com:443/wd/hub`)
   .withCapabilities(capabilities)
   .build();
- await driver.get('https://www.google.com');
+ await driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+// Test cases for orange hrm website 
+ await driver.findElement(webdriver.By.name("username")).sendKeys('Admin')
+ await driver.findElement(webdriver.By.name("password")).sendKeys('admin123')
+ await driver.findElement(webdriver.By.className("oxd-button oxd-button--medium oxd-button--main orangehrm-login-button")).click()
 
+ await driver.findElement(webdriver.By.className("oxd-icon-button oxd-main-menu-button")).click()
+ await driver.findElement(webdriver.By.className("oxd-userdropdown-img")).click()
+ await driver.findElement(webdriver.By.linkText("Logout")).click()
+ driver.wait(webdriver.until.urlContains('auth/login'), 1000);
   console.log( await driver.getTitle())
+
 
     await driver.quit();
   }
@@ -26,7 +30,7 @@ async function runTestWithCaps (capabilities) {
 // Define the capabilities of the browser you want to test on
 const capabilities = {
   browserName: 'chrome',
-  platform: 'Windows 11',
+  platform: 'Windows 10',
   version: 'latest',
   'sauce:options': {
     username: process.env.USER_NAME,
